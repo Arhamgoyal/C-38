647995b6260f9486da9f93580f04d845a4c92772
Arhamgoyal <Arhamgoyal007@gmail.com>
2020-11-09 16:49:45 +0530

    Adding Track Images

--- sketch.js
+++ sketch.js
@@ -9,7 +9,17 @@ var database;
 var form, player, game;
 
 var cars, car1, car2, car3, car4;
+var track,car1i,car2i,car3i,car4i;
+var ground;
 
+function preload(){
+  track=loadImage("images/track.jpg");
+  car1i=loadImage("images/car1.png");
+  car2i=loadImage("images/car2.png");
+  car3i=loadImage("images/car3.png");
+  car4i=loadImage("images/car4.png");
+  ground=loadImage("images/ground.png");
+}
 
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
@@ -27,5 +37,8 @@ function draw(){
   if(gameState === 1){
     clear();
     game.play();
+  }
+  if(gameState===2){
+    game.end();
   }
 }
